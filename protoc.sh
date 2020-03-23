dfcb95ae42ed4ab9634db4114c07324807f537f1
ethan <ethan.totten@gmail.com>
2020-03-22 20:58:11 -0700

    addin the needed proto files

--- protoc.sh
+++ protoc.sh
@@ -1,4 +1,4 @@
 protoc --go_out=plugins=grpc:. *.proto && mv github.com/thethan/fdr_proto/* . && rm -r github.com
 
 
-protoc --js_out=import_style=commonjs:. --grpc-web_out=import_style=commonjs,mode=grpcwebtext:.  *.proto
+protoc  --js_out=import_style=commonjs:../MDB-FDR/fdr_proto_js --grpc-web_out=import_style=commonjs,mode=grpcwebtext:../MDB-FDR/fdr_proto_js  *.proto
